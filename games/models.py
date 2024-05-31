4c136501f770e1dfa242bbfad3bf472fd8069c2f
Maria Alice <maria_alice_172@hotmail.com>
2024-05-31 20:45:58 -0300

    feat: add games model

--- games/models.py
+++ games/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from teams.models import Team
 
-# Create your models here.
+class Game(models.Model):
+    game_id = models.AutoField(primary_key=True)
+    team1 =  models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team1_games')
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team2_games')
+    team1_score = models.IntegerField()
+    team2_score = models.IntegerField()
+    round_choices = [
+        ('qualifying', 'Qualifying'),
+        ('quarter-final', 'Quarter-Final'),
+        ('semi-final', 'Semi-Final'),
+        ('final', 'Final'),
+        
+    ]
+    round = models.CharField(max_length=20, choices=round_choices)
