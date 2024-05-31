d0d7129f9b79202c5c918f7fdf4cf3cc7a453c98
Maria Alice <maria_alice_172@hotmail.com>
2024-05-31 20:57:33 -0300

    feat: add login details model

--- logins/models.py
+++ logins/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Login(models.Model):
+    login_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='logins')
+    login_time = models.DateTimeField(auto_now_add=True)
+    logout_time = models.DateTimeField(null=True, blank=True)
