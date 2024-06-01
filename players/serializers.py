3ac66872a9f0048b9ee909b4beee02a7ed2db32f
Maria Alice <maria_alice_172@hotmail.com>
2024-05-31 22:54:10 -0300

    fix: rename serializer player class

--- players/serializers.py
+++ players/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Player
 
 
-class TeamSerializer(serializers.ModelSerializer):
+class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = ['player_id', 'team', 'name', 'height', 'average_score', 'games_played']
