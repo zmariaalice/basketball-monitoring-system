4d233507a700a3fe8ba4239137ad21f4c5d965aa
Maria Alice <maria_alice_172@hotmail.com>
2024-06-02 03:24:35 -0300

    fix: as_view fixed

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from .views import UserListCreateView, UserDetailView
 
 urlpatterns = [
     path('', UserListCreateView.as_view(), name='user-list-create'),
-    path('int:pk/', UserDetailView.as_view, name='user-detail')
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail')
 ]
